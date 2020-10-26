442042f875f57962dce9028863f176799b79af14
Johnny Wesley <johnnyparente1245@hotmail.com>
2020-10-26 00:07:48 -0300

    Inicio Pronto

---
+++ Documents/NetBeansProjects/clinicacr/src/model/bean/Paciente.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author johnn
+ */
+public class Paciente {
+    
+    private int id;
+    private String  nome;
+    private int turno;
+    private String medico;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
